47a9b2860acc6bfd97090ecff9fe4e73b4cbc0c1
Chris <Chris.Inkpen@gmail.com>
2017-05-07 12:16:23 +0100

    Added Some colour to the circles

--- src/app/fractal-generator.service.ts
+++ src/app/fractal-generator.service.ts
@@ -11,17 +11,23 @@ export class FractalGeneratorService {
   public latestValue: Observable<Point>;
   timer;
   intervalMS = 1;
-  a: Point; b: Point; c: Point; startingPosition: Point; currentPosition: Point;
+  a: Point; b: Point; c: Point; d: Point; e: Point; f: Point; startingPosition: Point; currentPosition: Point;
   observer: Observer<Point>;
 
   constructor() { }
   ngOnInit() {
   }
 
-  init(a: Point, b: Point, c: Point, startingPosition: Point) {
+  init(a: Point, b: Point, c: Point,
+    d: Point, e: Point, f: Point,
+    startingPosition: Point) {
     this.a = a;
     this.b = b;
     this.c = c;
+    this.d = d;
+    this.e = e;
+    this.f = f;
+
     this.startingPosition = startingPosition;
     this.currentPosition = startingPosition;
 
@@ -33,25 +39,44 @@ export class FractalGeneratorService {
 
   updatePosition() {
     let res = this.throwDice();
-    switch (res) {
-      case Dice.ONE: {
-        this.currentPosition.x = this.currentPosition.x + ((this.a.x - this.currentPosition.x) / 2);
-        this.currentPosition.y = this.currentPosition.y + ((this.a.y - this.currentPosition.y) / 2);
 
+    let newA = new Point(this.currentPosition.x + ((this.a.x - this.currentPosition.x) / 2), this.currentPosition.y + ((this.a.y - this.currentPosition.y) / (2)));
+    let newB = new Point(this.currentPosition.x + ((this.b.x - this.currentPosition.x) / 2), this.currentPosition.y + ((this.b.y - this.currentPosition.y) / (2)));
+    let newC = new Point(this.currentPosition.x + ((this.c.x - this.currentPosition.x) / 2), this.currentPosition.y + ((this.c.y - this.currentPosition.y) / (2)));
+
+    let newD = new Point(this.currentPosition.x + ((this.d.x - this.currentPosition.x) / 2), this.currentPosition.y + ((this.d.y - this.currentPosition.y) / (2)));
+    let newE = new Point(this.currentPosition.x + ((this.e.x - this.currentPosition.x) / 2), this.currentPosition.y + ((this.e.y - this.currentPosition.y) / (2)));
+    let newF = new Point(this.currentPosition.x + ((this.f.x - this.currentPosition.x) / 2), this.currentPosition.y + ((this.f.y - this.currentPosition.y) / (2)));
+    switch (res) {
+      case Dice.A: {
+        this.currentPosition = newA;
         //Move half way towards A
         break;
       }
-      case Dice.TWO: {
+      case Dice.B: {
         //Move half way towards B
-        this.currentPosition.x = this.currentPosition.x + ((this.b.x - this.currentPosition.x) / 2);
-        this.currentPosition.y = this.currentPosition.y + ((this.b.y - this.currentPosition.y) / 2);
+        this.currentPosition = newB;
         break;
 
       }
-      case Dice.THREE: {
+      case Dice.C: {
         //Move half way towards C
-        this.currentPosition.x = this.currentPosition.x + ((this.c.x - this.currentPosition.x) / 2);
-        this.currentPosition.y = this.currentPosition.y + ((this.c.y - this.currentPosition.y) / 2);
+        this.currentPosition = newC;
+        break;
+
+      }
+      case Dice.D: {
+        this.currentPosition = newD;
+        break;
+
+      }
+      case Dice.E: {
+        this.currentPosition = newE;
+        break;
+
+      }
+      case Dice.F: {
+        this.currentPosition = newF;
         break;
 
       }
@@ -75,15 +100,27 @@ export class FractalGeneratorService {
 
 
   throwDice(): Dice {
-    let result = Math.random() * 3;
-    if (result < 1)
-      return Dice.ONE;
+    let result = Math.random() * 5;
+    if (result < 0) {
+      result = Math.random() * 3;
+      if (result < 1)
+        return Dice.D;
+      else if (result < 2)
+        return Dice.E;
+      else
+        return Dice.F;
+    }
+    else {
+      result = Math.random() * 3;
+      if (result < 1)
+        return Dice.A;
+      else if (result < 2)
+        return Dice.B;
+      else
+        return Dice.C;
+    }
 
-    else if (result < 2)
-      return Dice.TWO;
 
-    else
-      return Dice.THREE;
   }
 
 }
@@ -98,7 +135,5 @@ export class Point {
 
 
 export enum Dice {
-  ONE,
-  TWO,
-  THREE
+  A, B, C, D, E, F
 }
--- src/app/fractal-viewer/fractal-viewer.component.ts
+++ src/app/fractal-viewer/fractal-viewer.component.ts
@@ -21,13 +21,29 @@ export class FractalViewerComponent implements OnInit {
   ngOnInit(){
     let width = 900;
     let height = 900;
-    this.fractalGeneratorService.init(new Point(width/2,0), new Point(width,height), new Point(0,height), new Point(width/2, height/2));
+    let a = new Point(width/2,0);
+    let b = new Point(0,height)
+    let c = new Point(width, height);
+
+    let d = new Point(100, 2*height/4);
+    let e = new Point(width/2-50, 2*height/4+200);
+    let f = new Point(width/2, height-100);
+    
+    this.fractalGeneratorService.init(a,b,c,
+    d,e,f,
+     new Point(width/2, height/2));
 
     this.subscription = this.fractalGeneratorService.latestValue.
                           map(p=> JSON.parse(JSON.stringify(p))).
                           subscribe(p=> {
                             this.points.push(p);
                           });
+  }
+
+  calcRGB(x,y){
+    let r = Math.round(x/900 * 255);
+    let g = Math.round(y/900 * 255);
+    return "rgba(" + r + "," + g + ",200, 1)"
   }
 
 }
